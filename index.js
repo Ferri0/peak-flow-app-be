a2d6f5cbe8c37d4fc3b0f50be064df294c06a86a
Yaroslav Abrasimov <y.abrasimov@atwix.com>
2023-03-04 10:45:00 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+import fetch from "node-fetch";
+
+import express from 'express'
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World! 123')
+})
+
+app.get('/users', async (req, res) => {
+  const data = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+  const x = await data.json()
+
+  res.status(200).json(x)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
