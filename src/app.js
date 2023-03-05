09b63230cc58b40007bf8e2964614ddda974b6e8
Yaroslav Abrasimov <y.abrasimov@atwix.com>
2023-03-05 08:49:24 +0100

    Implement test endpoint

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import fetch from 'node-fetch';
 
 import authRouter from "./routers/authRouter.js";
 
@@ -10,6 +11,12 @@ const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('It works!')
+})
+
+app.get('/test-endpoint', async (req, res) => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+  const data = await response.json()
+  res.send(data)
 })
 
 app.listen(port, () => {
